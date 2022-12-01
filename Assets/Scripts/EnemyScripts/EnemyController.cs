93cd2fb44b3e5318e6bdfebf480bc775f39290e6
Trevor Hosek <trevorhosek@gmail.com>
2022-12-01 17:23:40 -0500

    Added enemy view and flinching

--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -8,44 +8,64 @@ public class EnemyController : MonoBehaviour, IDamageable
     protected float _maxHealth;
     [SerializeField]
     protected bool _isDestroyed;
+    [SerializeField]
+    protected float baseFlinchDuration;
+    [SerializeField]
+    protected float flinchDistance;
     public float maxHealth { 
-            get {return _maxHealth;}
-            set {_maxHealth = value;}
+        get {return _maxHealth;}
+        set {_maxHealth = value;}
         }
     public bool isDestroyed { 
-            get {return _isDestroyed;}
-            set {
-                _isDestroyed = value;
-                if(_isDestroyed) {
-                    transform.Rotate(new Vector3(0,0,90));
-                    Vector3 newPos = transform.position;
-                    newPos.y = .5f;
-                    transform.position = newPos;
-                    GetComponent<Collider>().enabled = false;
-                    }
+        get {return _isDestroyed;}
+        set {
+            _isDestroyed = value;
+            if(_isDestroyed) {
+                transform.Rotate(new Vector3(0,0,90));
+                Vector3 newPos = transform.position;
+                newPos.y = .5f;
+                transform.position = newPos;
+                GetComponent<Collider>().enabled = false;
                 }
+            }
         }
 
     protected float _health;
     public float health {
         get { return _health; }
         set { 
-                _health = Mathf.Clamp(value, 0, maxHealth); 
-                if(_health == 0) {
-                    isDestroyed = true;
-                } else {
-                    isDestroyed = false;
+            if(value < _health) {
+                StartCoroutine(Flinch(baseFlinchDuration));  
+            }
+            _health = Mathf.Clamp(value, 0, maxHealth); 
+            if(_health == 0) {
+                isDestroyed = true;
+            } else {
+                isDestroyed = false;
                 }
             }
         }
-    void Start()
+    private void Start()
     {
         _health = maxHealth;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+    }
+
+    private IEnumerator Flinch(float flinchDuration)
+    {
+        float start = Time.time;
+        float startX = transform.position.x;
+        float finalX = transform.position.x + flinchDistance;
+        while (Time.time < start + flinchDuration) {
+            float x = Mathf.Lerp(startX, finalX, (Time.time - start) / flinchDuration);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = new Vector3(finalX, transform.position.y, transform.position.z);
     }
 }
